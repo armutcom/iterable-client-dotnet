044b5d9bda722f39b6e304f15355855d8b35ce0b
akselarzuman <akselarzuman@gmail.com>
2018-10-16 17:00:00 +0300

    * Test project created * GetContentAsync tests added

---
+++ src/Client.Tests/Base/BaseTestClass.cs
@@ -0,0 +1,47 @@
+﻿using Armut.Iterable.Client.Contracts;
+using Armut.Iterable.Client.Core;
+using Moq;
+using Moq.Protected;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Armut.Iterable.Client.Tests.Base
+{
+    public class BaseTestClass
+    {
+        public Mock<HttpMessageHandler> MockHttpMessageHandler { get; set; }
+
+        public IRestClient CreateRestClient()
+        {
+            return new RestClient(new HttpClient
+            {
+                BaseAddress = new Uri("https://api.iterable.com/")
+            });
+        }
+
+        public IRestClient CreateRestClient(HttpResponseMessage httpResponseMessage)
+        {
+            MockHttpMessageHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            MockHttpMessageHandler
+                .Protected()
+                // Setup the PROTECTED method to mock
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                // prepare the expected response of the mocked http call
+                .ReturnsAsync(httpResponseMessage)
+                .Verifiable();
+
+            var httpClient = new HttpClient(MockHttpMessageHandler.Object)
+            {
+                BaseAddress = new Uri("https://api.iterable.com/")
+            };
+
+            return new RestClient(httpClient);
+        }
+    }
+}
---
+++ src/Client.Tests/RestClientTests/GetContentAsyncTests.cs
@@ -0,0 +1,43 @@
+﻿using Armut.Iterable.Client.Contracts;
+using Armut.Iterable.Client.Core.Responses;
+using Armut.Iterable.Client.Tests.Base;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Armut.Iterable.Client.Tests.RestClientTests
+{
+    public class GetContentAsyncTests : BaseTestClass
+    {
+        [Fact]
+        public async Task Should_Throw_Should_Throw_ArgumentException_If_Path_Is_Null_Or_Empty()
+        {
+            IRestClient _restClient = CreateRestClient();
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => _restClient.GetContentAsync(null)).ConfigureAwait(false);
+            await Assert.ThrowsAsync<ArgumentException>(() => _restClient.GetContentAsync(string.Empty)).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task Should_Return_ApiResponse_With_Content()
+        {
+            const string path = "/api/users/disableDevice";
+            const string content = "test content";
+
+            IRestClient _restClient = CreateRestClient(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.Created,
+                Content = new StringContent(content)
+            });
+
+            ApiResponse apiResponse = await _restClient.GetContentAsync(path).ConfigureAwait(false);
+
+            Assert.NotNull(apiResponse);
+            Assert.Equal(HttpStatusCode.Created, apiResponse.HttpStatusCode);
+            Assert.Equal(0, apiResponse.Headers.Count);
+            Assert.Equal(content, apiResponse.Content);
+        }
+    }
+}
